fb1cbffa9c647849b0cf33a37261ba643a94fdf8
caique camargo <caique.de.camargo@hotmail.com>
2020-04-13 14:47:28 -0300

    criando um servidor de arquivos

--- src/main/java/br/edu/ufabc/sged/endpoint/ItemEndpoint.java
+++ src/main/java/br/edu/ufabc/sged/endpoint/ItemEndpoint.java
@@ -1,7 +1,9 @@
 package br.edu.ufabc.sged.endpoint;
 
+import java.io.File;
 import java.net.URI;
 
+import javax.servlet.ServletContext;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -9,14 +11,19 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.UriInfo;
 
 import br.edu.ufabc.sged.model.Item;
 
 @Path("/items")
 @Transactional
 public class ItemEndpoint {
+	
+	@Context UriInfo uriInfo;
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
@@ -39,8 +46,18 @@ public class ItemEndpoint {
 	public Response addItem(Item item) {
 		
 		item.id = null;
+//		String applicationPath = servletContext.getRealPath("");
+//        String uploadFilePath  = applicationPath + "uploads" + File.separator + item.id;
 		item.persist();
+		String location = uriInfo.getAbsolutePath() + "/uploads/" + item.id;
+		System.out.println(location);
 		
-		return Response.created(URI.create(String.format("/items/%d", item.id))).build();
+		File fileSaveDir = new File(location);
+        if(!fileSaveDir.exists()){
+        	System.out.println(fileSaveDir.mkdirs());
+//            fileSaveDir.mkdirs();
+        }
+		
+		return Response.created(URI.create(String.format("/items/%d", item.id))).entity(location).build();
 	}
 }
